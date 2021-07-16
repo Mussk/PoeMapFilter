be1f51a6fcac1927f6309ceae1230755bb1eb4b0
Mussk <sanja.vin1999@gmail.com>
2021-07-16 16:19:40 +0200

    initial commit

---
+++ Links.cs
@@ -0,0 +1,11 @@
+﻿namespace PoeMapFIlter
+{
+    public class Links
+    {
+       public static string GitHubPage = "https://github.com/Mussk/PoeMapFilter";
+
+        public static string BugReport = "https://github.com/Mussk/PoeMapFilter/issues";
+
+        public static string FontLink = "https://www.exljbris.com/fontin.html";
+    }
+}
